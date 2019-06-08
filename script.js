705e6bd26fc1f91b672e1dbc3dfd915c04a27710
brunoavce@gmail.com <brunoavce@gmail.com>
2019-06-08 17:39:31 -0300

    variável e operações matemáticas

--- script.js
+++ script.js
@@ -4,4 +4,15 @@ Curso: Introdução a liguagem JavaScript
 Autor: Bruno Vieira
 Data: 08/06/2019
 */
-alert("Olá mundo!");
+var a = 3;
+var b = 2;
+
+var soma = a+b;
+var sub = a-b;
+var mult = a*b;
+var div = a/b;
+
+alert(soma);
+alert(sub);
+alert(mult);
+alert(div);
