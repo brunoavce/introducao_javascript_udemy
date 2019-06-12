79be2748bab217335f8fb2759a6b95d669943194
brunoavce@gmail.com <brunoavce@gmail.com>
2019-06-12 18:49:50 -0300

    estruturas de condicionais

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 /*
-Hello world em JavaScript
-Curso: Introdução a liguagem JavaScript
-Autor: Bruno Vieira
-Data: 08/06/2019
+ESTRUTURAS CONDICIONAIS
 */
-var a = 3;
-var b = 2;
 
-var soma = a+b;
-var sub = a-b;
-var mult = a*b;
-var div = a/b;
+var a = 2;
+var b = 3;
+var c = "Olá mundo";
 
-alert(soma);
-alert(sub);
-alert(mult);
-alert(div);
+if (c%2 == 1){
+    alert("numero é impar");
+} 
+else if (c%2 == 0){
+    alert("numero é par")
+}
+else {
+    alert("valor inválido");
+}
