171ec48bc1b93cd5e3a5764ed8c4e9668ba340f7
brunoavce@gmail.com <brunoavce@gmail.com>
2020-05-13 17:41:50 -0300

    teste

---
+++ script_jquery.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('#buttonp').click(function(){
+        $('.pessoa').css("color", "blue");
+    });
+
+    $('#buttond').click(function(){
+        $('.divindade').css("color", "red");
+    });
+
+    $('#buttonj').click(function(){
+        $('.juncao').css("color", "green");
+    });
+    
+})
